d0151a320c0cad32b4c8342125a50fa2b44aac88
Tony <tony.verdin@gmail.com>
2020-01-29 03:42:54 -0600

    Post Scaffolding

---
+++ PizzaBox.DataAccess/Models/Addressing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaBox.DataAccess.Models
+{
+    public partial class Addressing
+    {
+        public Addressing()
+        {
+            Customer = new HashSet<Customer>();
+            Store = new HashSet<Store>();
+        }
+
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Street { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Customer> Customer { get; set; }
+        public virtual ICollection<Store> Store { get; set; }
+    }
+}
