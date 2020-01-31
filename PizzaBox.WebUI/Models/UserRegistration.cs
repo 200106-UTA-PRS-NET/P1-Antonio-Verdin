cac89376bee371c6d5fc8e5210cee67c8201459f
Tony <tony.verdin@gmail.com>
2020-01-31 03:18:48 -0600

    Registration Page Added

---
+++ PizzaBox.WebUI/Models/UserRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaBox.WebUI.Models
+{
+    public class UserRegistration
+    {
+        public string Firsname { get; set; }
+        public string Lastname { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string phone { get; set; }
+        public string address1 { get; set; }
+        public string street { get; set; }
+
+    }
+}
